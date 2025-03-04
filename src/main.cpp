ccdeecb3fcb078c8220c8f682e974abb50fa65e3
Ahmed Mohamed Fahmy Eldawy Hamed <185392994+Ahmed-Fahmy00@users.noreply.github.com>
2025-03-04 13:27:44 +0200

    main file

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <conio.h>
+#include "Task3.cpp"
+
+using namespace std;
+
+int main()
+{
+    cout << "\t hello this is task 3 \n";
+    cout << "\t choose 1 option: \n";
+    cout << "\t 1- Original given question \n";
+    cout << "\t 2- Change question \n";
+    cout << "\t 3- exit \n";
+
+    char choice;
+    while (true) {
+        choice = _getch();
+
+        switch (choice) {
+            case '1':
+                cout << "Original given question \n";
+
+                break;
+            case '2':
+                cout << "Change question \n";
+
+                break;
+            case '3':
+                cout << "Exiting program \n";
+                return 0;
+            default:
+                break;
+        }
+    }
+}
