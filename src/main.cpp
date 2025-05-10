7b54cbb1b139b631261e0e88b4044ba9f1ae565b
Ahmed Mohamed Naguib <amnoe20052007@gmail.com>
2025-05-10 16:16:10 +0300

    Tiny input changes 2.0

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,7 @@
 using namespace std;
 
 int main() {
-    cout << "Enter 0/1: ";
-    int choice;
-    cin >> choice;
+    int choice = 1;
     while (choice == 1) {
         cout <<"Enter the value of n for the n x n grid ( n > 2 ): ";
         int n;
@@ -14,11 +12,17 @@ int main() {
             cout << "Invalid input. Please enter a value greater than 2." << endl;
         }else {
             PointLattice lattice(n);
-            lattice.solveGreedy();
-            //lattice.solveDP();
+            cout << "Enter 1 to solve using Greedy, 2 to solve using Dynamic Programming: ";
+            int method;
+            cin >> method;
+            if (method == 1) {
+                lattice.solveGreedy();
+            } else {
+                lattice.solveDP();
+            }
             cout << "Minimum moves required: " << lattice.moves << endl;
         }
-        cout << "Enter 0/1: ";
+        cout << "Enter 1 to solve the problem, 0 to exit: ";
         cin >> choice;
     }
 
